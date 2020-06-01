48cdc6c33a12e20b5aa669c4ae289e40effcfa1d
rveigarcia <rveigarcia@gmail.com>
2020-06-01 23:12:53 +0200

    subiendo comentarios

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -3,8 +3,5 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by jt on 6/13/17.
- */
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
