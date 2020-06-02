91777a82648d524006231172b137765fc432b7fb
rveigarcia <rveigarcia@gmail.com>
2020-06-02 16:26:07 +0200

    subiendo comentarios

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Slf4j // para emplear el Logger
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
