ed8af7e43b9fdd3bfef12f4d2ca0cc32df6c75c3
rveigarcia <rveigarcia@gmail.com>
2020-06-02 19:15:04 +0200

    subiendo comentarios

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -14,9 +14,6 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-/**
- * Created by jt on 6/17/17.
- */
 public class RecipeServiceImplTest {
 
     RecipeServiceImpl recipeService;
